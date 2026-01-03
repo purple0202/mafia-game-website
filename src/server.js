d0443a44a35105e448b6d660944ac419ef441b72
Vince Park <vincepark222@gmail.com>
2026-01-03 22:34:46 +1100

    socketIO setup serverside

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
@@ -5,3 +7,19 @@ const app = express();
 console.log("hello")
 
 app.listen("3000")
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (_,res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
+
+const handleListen = () => console.log('Listening on localhost:3000');
+
+const httpServer = http.createServer(app);
+const io = SocketIO(httpServer);
+
+function onSocketClose() {
+    console.log("Disconnected from the Browser!");
+}
+
+httpServer.listen(3000, handleListen);
