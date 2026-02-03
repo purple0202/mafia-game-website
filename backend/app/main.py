4b4937e34196c486e98b68e2929b07b310d3a2b2
Vince Park <vincepark222@gmail.com>
2026-02-03 22:43:33 +1100

    added main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,12 @@
 from fastapi import FastAPI
+from app.auth.routes import router as auth_router
+from app.games.routes import router as game_router
+from app.sockets.mafia import sio_app
+
+app = FastAPI(title="Mafia Game API")
+
+app.include_router(auth_router, prefix="/auth")
+app.include_router(game_router, prefix="/games")
+
+# Mount Socket.IO
+app.mount("/ws", sio_app)
